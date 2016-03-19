fafe5f156e2bd4da920e69c72beca2323e43b470
uuuaaa <yuoya@softbank.ne.jp>
2016-03-19 18:36:30 +0900

    Initial Commit

---
+++ cardSlide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cardSlide
+//
+//  Created by 大屋雄 on 2016/03/19.
+//  Copyright © 2016年 Yu Oya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
